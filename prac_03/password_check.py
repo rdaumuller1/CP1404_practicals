f8a961fa1290a33fe803b5c2bcd588b8ff16ad7a
jc427738 <renee.daumuller@my.jcu.edu.au>
2019-03-21 14:15:10 +1000

    Add password check program

---
+++ prac_03/password_check.py
@@ -0,0 +1,10 @@
+
+MIN_LENGTH = 3
+
+password = input("Password: ")
+
+while MIN_LENGTH >= 0:
+    if len(password) >= MIN_LENGTH:
+        print(len(password) * '*')
+    password = input("Password: ")
+
