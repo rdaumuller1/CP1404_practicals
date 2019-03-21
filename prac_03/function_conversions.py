da386af32b21857a807f85fd07813c171ad71aa5
jc427738 <renee.daumuller@my.jcu.edu.au>
2019-03-21 14:44:27 +1000

    Add 2 programs from prac_01

--- prac_03/function_conversions.py
+++ prac_03/function_conversions.py
@@ -4,17 +4,39 @@ If sales are under $1,000, the user gets a 10% bonus.
 If sales are $1,000 or over, the bonus is 15%.
 """
 
-sales = float(input("Enter sales: $"))
-if sales < 1000:
-    bonus = sales * 0.1
-else:
-    bonus = sales * 0.15
-print("Bonus = ", bonus)
 
-# number_of_items = float(input("Number of Items: "))
-# total_price = 0
-# while number_of_items > 0:
-#         price_of_item = float(input("Price of item: "))
-#         total_price = total_price + price_of_item
-#         number_of_items = number_of_items - 1
-# print ("Total Price for 3 Items:", total_price)
+def main():
+    MENU = """C - Convert Celsius to Fahrenheit
+    F - Convert Fahrenheit to Celsius
+    Q - Quit"""
+    print(MENU)
+    choice = input(">>> ").upper()
+
+
+    while choice != "Q":
+        if choice == "C":
+            celsius = float(input("Celsius: "))
+            fahrenheit = celsius * 9.0 / 5 + 32
+            print("Result: {:.2f} F".format(fahrenheit))
+        elif choice == "F":
+            fahrenheit = float(input("Fahrenheit: "))
+            celsius = 5 / 9 * (fahrenheit - 32)
+            print("Result: {:.2f} C".format(celsius))
+        else:
+            print("Invalid option")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Thank you.")
+
+
+main()
+
+# score = float(input("Enter score: "))
+# if score < 0 or score > 100:
+#     print("Invalid Score")
+# elif score < 50:
+#     print("Bad")
+# elif score < 90:
+#     print("Passable")
+# else:
+#     print("Excellent")
