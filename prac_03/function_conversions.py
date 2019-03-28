416d6d47066b06881274cf9ec6d8db209a98ab85
jc427738 <renee.daumuller@my.jcu.edu.au>
2019-03-28 10:25:42 +1000

    show previous code with functions built in.

--- prac_03/function_conversions.py
+++ prac_03/function_conversions.py
@@ -1,42 +1,53 @@
-"""
-Program to calculate and display a user's bonus based on sales.
-If sales are under $1,000, the user gets a 10% bonus.
-If sales are $1,000 or over, the bonus is 15%.
-"""
-
-
-def main():
-    MENU = """C - Convert Celsius to Fahrenheit
-    F - Convert Fahrenheit to Celsius
-    Q - Quit"""
-    print(MENU)
-    choice = input(">>> ").upper()
-
-
-    while choice != "Q":
-        if choice == "C":
-            celsius = float(input("Celsius: "))
-            fahrenheit = celsius * 9.0 / 5 + 32
-            print("Result: {:.2f} F".format(fahrenheit))
-        elif choice == "F":
-            fahrenheit = float(input("Fahrenheit: "))
-            celsius = 5 / 9 * (fahrenheit - 32)
-            print("Result: {:.2f} C".format(celsius))
-        else:
-            print("Invalid option")
-        print(MENU)
-        choice = input(">>> ").upper()
-    print("Thank you.")
-
-
-main()
-
-# score = float(input("Enter score: "))
-# if score < 0 or score > 100:
-#     print("Invalid Score")
-# elif score < 50:
-#     print("Bad")
-# elif score < 90:
-#     print("Passable")
-# else:
-#     print("Excellent")
+# MENU = """C - Convert Celsius to Fahrenheit
+# F - Convert Fahrenheit to Celsius
+# Q - Quit"""
+#
+#
+# def main():
+#     print(MENU)
+#     choice = input(">>> ").upper()
+#
+#     while choice != "Q":
+#         if choice == "C":
+#             celsius = float(input("Celsius: "))
+#             fahrenheit = celsius_to_fahrenheit_conversion(celsius)
+#             print("Result: {:.2f} F".format(fahrenheit))
+#         elif choice == "F":
+#             fahrenheit = float(input("Fahrenheit: "))
+#             celsius = fahrenheit_to_celsius_conversion(fahrenheit)
+#             print("Result: {:.2f} C".format(celsius))
+#         else:
+#             print("Invalid option")
+#         print(MENU)
+#         choice = input(">>> ").upper()
+#     print("Thank you.")
+#
+#
+# def celsius_to_fahrenheit_conversion(celsius):
+#     return celsius * 9 / 5 + 32
+#
+#
+# def fahrenheit_to_celsius_conversion(fahrenheit):
+#     return 5 / 9 * (fahrenheit - 32)
+#
+#
+# main()
+#
+#
+# def main():
+#     score = float(input("Enter score: "))
+#     print(catergorise_score(score))
+#
+#
+# def catergorise_score(score):
+#     if score < 0 or score > 100:
+#         print("Invalid Score")
+#     elif score < 50:
+#         print("Bad")
+#     elif score < 90:
+#         print("Passable")
+#     else:
+#         print("Excellent")
+#
+#
+# main()
